772a153402e6cfb1fb4caccb89d25c0d8d7b4e1b
carmine <carmin01@libero.it>
2022-11-05 13:34:20 +0100

    rivisita alla classe disable_file_system_redirection

--- Bot/systemRetrieval.py
+++ Bot/systemRetrieval.py
@@ -8,16 +8,16 @@ from datetime import datetime
 
 # Classe sottostante completamente copiata, serve per far leggere alcune directory che hanno un problema, per via di
 # windows che fa funziona un "redirector", non so che spaccetta sia, capiamo.
-
-class disable_file_system_redirection:
-    _disable = ctypes.windll.kernel32.Wow64DisableWow64FsRedirection
-    _revert = ctypes.windll.kernel32.Wow64RevertWow64FsRedirection
-    def __enter__(self):
-        self.old_value = ctypes.c_long()
-        self.success = self._disable(ctypes.byref(self.old_value))
-    def __exit__(self, type, value, traceback):
-        if self.success:
-            self._revert(self.old_value)
+if os.name == "nt":
+    class disable_file_system_redirection:
+        _disable = ctypes.windll.kernel32.Wow64DisableWow64FsRedirection
+        _revert = ctypes.windll.kernel32.Wow64RevertWow64FsRedirection
+        def __enter__(self):
+            self.old_value = ctypes.c_long()
+            self.success = self._disable(ctypes.byref(self.old_value))
+        def __exit__(self, type, value, traceback):
+            if self.success:
+                self._revert(self.old_value)
 
 def get_size(B):
     """
